b6142a1137011567b723cf0ad9b226ae90107448
Singh Pablo <pablo22_4@hotmail.com>
2022-09-28 12:11:52 -0300

    15

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const routes = require('./src/routes/index2.js');
 const { db } = require('./src/db.js');
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => res.send('Hello World! 3'));
 
