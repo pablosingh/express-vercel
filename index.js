3cd0dbd9d0b7c3cabc35858b5650a1f30f79c846
Singh Pablo <pablo22_4@hotmail.com>
2022-09-28 09:54:41 -0300

    sin enrutador 2

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => res.send('Hello World! Pablo'));
 
 app.use( '/', routes );
 app.get('/get', (req,res)=> res.json({ msg: "ok" }));
+app.get('/get2', (req,res)=> res.send('get 2'));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
